cb2278b50cb2360d1c81295b858f5c08417d1a39
Cristhian Sevilla <crissev17@gmail.com>
2023-06-16 21:15:10 -0600

    Consultando la API para traer las noticias sengun la categoria seleccionada

--- src/components/context/NoticiasProvider.jsx
+++ src/components/context/NoticiasProvider.jsx
@@ -1,16 +1,39 @@
-import {useState, useEffect, createContext} from 'react'
+import { useState, useEffect, createContext } from 'react'
+import axios from 'axios'
 
 const NoticiasContext = createContext()
 
-const NoticiasProvider = ({children}) => {
+const NoticiasProvider = ({ children }) => {
 
-    const [categoria, setCategoria] =useState('general')
+    const [categoria, setCategoria] = useState('general')
+    const [noticias, setNoticias] = useState([])
 
     const handleChangeCategoria = e => {
         setCategoria(e.target.value)
     }
 
-    return(
+    //Consumir API
+    useEffect(() => {
+
+        try {
+            const consultarAPI = async () => {
+                const url = `https://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&apikey=${import.meta.env.VITE_API_KEY}`
+
+                const { data } = await axios(url)
+
+                setNoticias(data.articles)
+
+            }
+
+            consultarAPI()
+
+        } catch (error) {
+            console.log(error);
+        }
+
+
+    }, [categoria])
+    return (
         <NoticiasContext.Provider
             value={{
                 categoria,
@@ -23,7 +46,7 @@ const NoticiasProvider = ({children}) => {
     )
 }
 
-export{
+export {
     NoticiasProvider
 }
 
